cab2be57056dad9e6c1ba5da0d168fe96c014254
norbuTech <code.norbutech@gmail.com>
2024-05-15 17:25:32 +0600

    sorce code

--- athangflutterapp/lib/main.dart
+++ athangflutterapp/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Mobile App Development',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
